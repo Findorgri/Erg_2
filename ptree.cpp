7a310be54b57672318463f28c80cd3447179746a
Findorgri <christianobusiness@hotmail.com>
2016-11-30 00:17:59 +0200

    Update ptree.cpp

--- ptree.cpp
+++ ptree.cpp
@@ -2,60 +2,142 @@
 #include <cstring>
 #include <cstdio>
 
-int SerialNumber = 0;
+int serialNumberCounter = 0;
+const char *nodeLabels[] = { "NT_NUMBER", "NT_IDENTIFIER",
+"NT_ADDITION", "NT_SUBTRACTION", "NT_MULTIPLICATION", "NT_DIVISION",
+"NT_ASSIGNMENT", "NT_UNARYPLUS", "NT_UNARYMINUS", "NT_STATEMENT", "NT_COMPILATIONUNIT" };
+struct ptnode *root;
+struct ptnode *symbols[50] = { NULL };
 
-struct ptnode* root;
+struct ptnode *NewNode(NODETYPE type,
+	struct ptnode *left, struct ptnode *right, char *yytext)
+{
+	char serial[30];
+	struct ptnode *newNode;
+	newNode = (struct ptnode *)malloc(sizeof(struct ptnode));
 
-const char* nodestrings[] = { "NUMBER", "addition_list", "addition" };
+	newNode->m_type = type;
+	newNode->m_serialNumber = serialNumberCounter++;
 
-struct ptnode* new_Node(NODETYPE n_type, struct ptnode* left, struct ptnode* right, char* yytext) {
+	newNode->m_leftChild = left;
+	newNode->m_rightChild = right;
 
-	struct ptnode* newNode = (struct ptnode*) malloc(sizeof(struct ptnode));
-
-	newNode->SerialNumber = SerialNumber++;
-
-	newNode->type = n_type;
-	newNode->left_child = left;
-	newNode->right_child = right;
-
-	newNode->graph_label = (char*)malloc(50);
-	strcpy(newNode->graph_label, nodestrings[n_type]);
-	strcat(newNode->graph_label, "_");
-	char buffer[30];
-	itoa(newNode->SerialNumber, buffer, 10);
-	strcat(newNode->graph_label, buffer);
-
-	if (yytext != NULL) {
-
-		newNode->semanticValue = atoi(yytext);
-
+	if (type == NT_IDENTIFIER) {
+		newNode->m_identifier = strdup(yytext);
 	}
 	else {
+		newNode->m_identifier = NULL;
+	}
 
-		newNode->semanticValue = -1;
+	newNode->m_label = (char *)malloc(50);
+	strcpy(newNode->m_label, nodeLabels[type]);
+	strcat(newNode->m_label, "_");
+	itoa(newNode->m_serialNumber, serial, 10);
+	strcat(newNode->m_label, serial);
 
+	if (yytext != NULL)
+	{
+		newNode->m_semanticValue = atoi(yytext);
+	}
+	else
+	{
+		newNode->m_semanticValue = -1;
 	}
 
 	return newNode;
-
 }
 
-void printTraversal(FILE * fp, struct ptnode * current_n)
+void printGraphViz(FILE *fp, struct ptnode *current)
 {
-
-	if (current_n->left_child != NULL) {
-
-		fprintf(fp, "\"%s\" -> \"%s\";\n", current_n->graph_label, current_n->left_child->graph_label);
-		printTraversal(fp, current_n->left_child);
-
+	if (current->m_leftChild != NULL)
+	{
+		fprintf(fp, "\"%s\" -> \"%s\";\n", current->m_label, current->m_leftChild->m_label);
+		printGraphViz(fp, current->m_leftChild);
 	}
+	if (current->m_rightChild != NULL)
+	{
+		fprintf(fp, "\"%s\" -> \"%s\";\n", current->m_label, current->m_rightChild->m_label);
+		printGraphViz(fp, current->m_rightChild);
+	}
+}
 
-	if (current_n->right_child != NULL) {
+struct ptnode *lookup(char *identifier, bool query)
+{
+	int index = 0;
+	struct ptnode *newnode;
+	struct ptnode *p = symbols[0];
 
-		fprintf(fp, "\"%s\" -> \"%s\";\n", current_n->graph_label, current_n->right_child->graph_label);
-		printTraversal(fp, current_n->right_child);
-
+	while (p != NULL) {
+		// i strcmp epistrefei 0 gia isotita 
+		if (!strcmp(identifier, p->m_identifier)) {
+			return p;
+		}
+		// I swapped the two following statements and worked
+		index++;
+		p = symbols[index];
+	}
+	if (query) {
+		return NULL;
+	}
+	else {
+		newnode = NewNode(NT_IDENTIFIER, NULL, NULL, identifier);
+		symbols[index] = newnode;
+		return newnode;
 	}
 
 }
 
+int eval(struct ptnode *current)
+{
+	int result;
+	struct ptnode *lhs;
+
+	if (current->m_type == NT_COMPILATIONUNIT) {
+		eval(current->m_leftChild);
+		if (current->m_rightChild != NULL) {
+			eval(current->m_rightChild);
+		}
+
+		return 0;
+	}
+	else if (current->m_type == NT_STATEMENT) {
+		if (current->m_leftChild != NULL) {
+			printf("Result = %d\n", eval(current->m_leftChild));
+		}
+
+		return 0;
+	}
+	else if (current->m_type == NT_ADDITION) {
+		return eval(current->m_leftChild) + eval(current->m_rightChild);
+	}
+	else if (current->m_type == NT_SUBTRACTION) {
+		return eval(current->m_leftChild) - eval(current->m_rightChild);
+	}
+	else if (current->m_type == NT_MULTIPLICATION) {
+		return eval(current->m_leftChild) * eval(current->m_rightChild);
+	}
+	else if (current->m_type == NT_DIVISION) {
+		if (result = eval(current->m_rightChild) != 0) {
+			return eval(current->m_leftChild) / result;
+		}
+		else {
+			printf("kara....!!#!$@#$!@#@");
+		}
+	}
+	else if (current->m_type == NT_UNARYMINUS) {
+		return -eval(current->m_leftChild);
+	}
+	else if (current->m_type == NT_UNARYPLUS) {
+		return eval(current->m_leftChild);
+	}
+	else if (current->m_type == NT_NUMBER) {
+		return current->m_semanticValue;
+	}
+	else if (current->m_type == NT_IDENTIFIER) {
+		return current->m_semanticValue;
+	}
+	else if (current->m_type == NT_ASSIGNMENT) {
+		lhs = lookup(current->m_leftChild->m_identifier, true);
+		return lhs->m_semanticValue = eval(current->m_rightChild);
+	}
+}
